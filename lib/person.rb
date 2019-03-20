2f8f4a780719343f7384b32be7ce4c1d70fea27b
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-20 21:03:48 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,12 @@
 # your code goes here
 class Person
-  attr_reader :name, :happiness 
+  attr_reader :name, :happiness, :hygiene
   attr_accessor :bank_account
   def initialize(name)
     @name = name
     @bank_account = 25
     @happiness = 8
+    @hygiene = 8
   end
   
   def happiness=(joy)
